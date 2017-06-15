d2cb67897c74fdc2c361144b3372d5e87d8264c2
myuon <ioi.joi.koi.loi@gmail.com>
2017-06-15 21:15:04 +0900

    need override?

--- SDLight/Widgets/Core.hs
+++ SDLight/Widgets/Core.hs
@@ -1,91 +1,107 @@
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE TypeFamilyDependencies #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE EmptyCase #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE RankNTypes #-}
 module SDLight.Widgets.Core where
 
-import Pipes
-import Control.Object
 import Control.Monad.State.Strict
-import Control.Monad.Skeleton
-import Data.Functor.Identity
+import Control.Object
+import Data.Proxy
 import Data.Functor.Sum
 
-type Widget eff m r = Monad m => forall r. Pipe (eff m r) r m ()
+statefulM :: Monad m => (forall a. t a -> StateT s m a) -> m s -> Object t m
+statefulM h = go where
+  go s = Object $ \f -> s >>= runStateT (h f) >>= \(a, s') -> s' `seq` return (a, go (return s'))
+
+infix 1 @@~
+(@@~) :: Monad m => m s -> (forall a. t a -> StateT s m a) -> Object t m
+m @@~ f = statefulM f m
 
 --
 
-data Child1 = Child1 Int deriving (Eq, Show)
-data Child2 = Child2 String deriving (Eq, Show)
+data Union (r :: [* -> *]) v where
+  UNow  :: t v -> Union (t : r) v
+  UNext :: Union (t : r) v -> Union (any : t : r) v
 
-data Add = Add Int
+class Member (ts :: [* -> *]) (x :: * -> *) where
+  liftU :: (forall v. x v -> r v) -> (forall v. Union ts v -> r v) -> (forall v. Union ts v -> r v)
+  
+instance Member (t : ts) t where
+  liftU f g (UNow tv) = f tv
+  
+instance Member ts t => Member (any : ts) t where
+  liftU f g (UNow av) = g (UNow av)
+  liftU f g (UNext t) = liftU f (g . UNext) t
 
-data Eff'Child1 m r where
-  Render'Child1 :: Eff'Child1 IO ()
-  Step'Child1 :: Eff'Child1 IO Add
+class Include (ts :: [k]) (xs :: [k])
+instance Include ts '[]
+instance (Include ts xs, Member ts x) => Include ts (x : xs)
 
-objChild1 :: Monad m => Object (Eff'Child1 m) m
-objChild1 = stateful go (Child1 299) where
-  go :: Eff'Child1 m r -> StateT Child1 m r
-  go Render'Child1 = get >>= lift . print
-  go Step'Child1 = do
-    Child1 n <- get
-    put $ Child1 (n+1)
-    return $ Add n
+class CaseOf r t rs | r t -> rs where
+  caseOf :: Union r v -> Either (Union rs v) (t v)
 
-data Eff'Child2 m r where
-  Render'Child2 :: Eff'Child2 IO ()
-  Step'Child2 :: Eff'Child2 IO ()
+instance CaseOf (r : rs) r rs where
+  caseOf (UNow tv) = Right tv
+  caseOf (UNext n) = Left n
 
-objChild2 :: Monad m => Object (Eff'Child2 m) m
-objChild2 = stateful go (Child2 "poyo") where
-  go :: Eff'Child2 m r -> StateT Child2 m r
-  go Render'Child2 = get >>= lift . print
-  go Step'Child2 = do
-    Child2 n <- get
-    put $ Child2 (n ++ ".")
+infixr 2 @>
+(@>) :: (t v -> r) -> (Union ts v -> r) -> Union (t : ts) v -> r
+(@>) f g u = either g f $ caseOf u
 
---
+emptyUnion :: Union '[] v -> a
+emptyUnion = \case
 
-data Eff'Parent m r where
-  Render'Parent :: Eff'Parent IO ()
-  Step'Parent :: Eff'Parent IO ()
+type family (++) (a :: [k]) (b :: [k]) where
+  '[] ++ bs = bs
+  (a : as) ++ bs = a : (as ++ bs)
 
-data Parent m
-  = Parent
-  { count :: Int
-  , child1 :: Instance (Eff'Child1 m) m
-  , child2 :: Instance (Eff'Child2 m) m
-  }
+type Singleton x = Union '[x]
 
-objParent :: Monad m => Parent m -> Object (Eff'Parent m) m
-objParent = stateful go where
-  go :: Eff'Parent m r -> StateT (Parent m) m r
-  go Render'Parent = do
-    this <- get
-    lift $ print $ "parent render:"
-    lift $ print $ count this
-    lift $ print $ "children render:"
-    lift $ child1 this .- Render'Child1
-    lift $ child2 this .- Render'Child2
-  go Step'Parent = do
-    this <- get
-    Add n <- lift $ child1 this .- Step'Child1
-    modify $ \t -> t { count = count t + n }
+override :: Member xs x => (forall v. x v -> m v) -> Object (Union xs) (m `Sum` (Union xs))
+override f = _
 
-    lift $ child2 this .- Step'Child2
+{-
+type family Pred (n :: Nat) :: Nat where
+  Pred Z = Z
+  Pred (S n) = n
 
-    modify $ \t -> t { count = count t + 1 }
+proxyPred :: Proxy n -> Proxy (Pred n)
+proxyPred _ = Proxy
 
-main = do
-  ch1 <- new objChild1
-  ch2 <- new objChild2
-  parent <- new $ objParent $ Parent 5 ch1 ch2
+proxySucc :: Proxy n -> Proxy (S n)
+proxySucc _ = Proxy
 
-  parent .- Render'Parent
-  parent .- Step'Parent
-  parent .- Render'Parent
-  parent .- Step'Parent
-  parent .- Render'Parent
+type family Find (t :: * -> *) r :: Nat where
+  Find t (t : _) = Z
+  Find t (_ : r) = S (Find t r)
+
+class Member' t r (n :: Nat) where
+  inj' :: Proxy n -> t v -> Union r v
+  prj' :: Proxy n -> Union r v -> t v
+
+instance Member' t (t : r) Z where
+  inj' _ = UNow
+  prj' _ (UNow t) = t
+
+instance Member' t (r : r') n => Member' t (any : r : r') (S n) where
+  inj' p t = UNext (inj' (proxyPred p) t)
+  prj' p (UNext t) = prj' (proxyPred p) t
+
+class Member t r where
+  inj :: t v -> Union r v
+  prj :: Union r v -> t v
+-}
+
 
--- SDLight/Widgets/Layer.hs
+++ SDLight/Widgets/Layer.hs
@@ -1,3 +1,10 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE Rank2Types #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE LambdaCase #-}
@@ -27,10 +34,12 @@ import qualified SDL.Image as SDL
 import qualified Data.Map as M
 import Data.List
 import Data.Maybe
-import Control.Lens
+import Control.Arrow (first, second)
+import Control.Lens hiding ((:>))
 import Control.Monad
-import Control.Monad.State
-import Pipes
+import Control.Monad.State.Strict
+import Control.Object
+import Data.Functor.Sum
 import Linear.V2
 import SDLight.Types
 import SDLight.Widgets.Core
@@ -87,17 +96,18 @@ renderLayer layer pos = do
   let loc = SDL.Rectangle (SDL.P $ fmap toEnum pos) (SDL.V2 (layer^.layerWidth) (layer^.layerHeight))
   lift $ SDL.copy rend (layer^.layerTexture) Nothing (Just $ fmap toEnum $ loc)
 
-data Eff'Layer this m r where
-  New'Layer :: FilePath -> Int -> Int -> Eff'Layer this GameM this
-  Render'Layer :: this -> V2 Int -> Eff'Layer this GameM ()
+data Op'Render r where
+  Op'Render :: Layer -> V2 Int -> Op'Render ()
 
-wLayer :: Monad m => Widget (Eff'Layer Layer) m r
-wLayer = await >>= \e -> case e of
-  New'Layer path w h -> lift (newLayer path (V2 w h)) >>= yield
-  Render'Layer layer v -> lift (renderLayer layer v) >>= yield
+singleton :: (forall v. t v -> m v) -> (forall v. Singleton t v -> m v)
+singleton f = f @> emptyUnion
+
+wLayer :: FilePath -> V2 Int -> Object (Singleton Op'Render) GameM
+wLayer path v = newLayer path v @@~ singleton (\(Op'Render layer v) -> lift $ renderLayer layer v)
 
 -- Layered
 
+{-
 newtype Layered a = Layered (a, Layer)
 
 layered :: Lens' (Layered a) a
@@ -109,32 +119,29 @@ newLayered path v initA = Layered <$> liftM2 (,) initA (newLayer path v)
 renderLayered :: Layered a -> V2 Int -> (a -> GameM ()) -> GameM ()
 renderLayered (Layered (ma,layer)) pos k = renderLayer layer pos >> k ma
 
-data Eff'Layered eff this (m :: * -> *) r where
-  New'Layered :: FilePath -> V2 Int -> eff that GameM this -> Eff'Layered eff that GameM (Layered this)
-  Render'Layered :: V2 Int -> (this -> eff this GameM ()) -> Layered this -> Eff'Layered eff this GameM ()
-  Map'Layered :: (this -> eff that m this) -> Layered this -> Eff'Layered eff that m (Layered this)
+-}
 
-wfLayered :: Widget (eff that) m r -> Widget (Eff'Layered eff that) m r
-wfLayered widget = await >>= \case
-  New'Layered path v new ->
-    for (yield new >-> widget) $ \t -> lift (newLayered path v (return t)) >>= yield
-  Render'Layered v render this ->
-    for (yield (render (this^.layered)) >-> widget) $ \() -> lift (renderLayered this v (\_ -> return ())) >> yield ()
-  Map'Layered op this ->
-    for (yield (op (this^.layered)) >-> widget) $ \w -> yield $ this & layered .~ w
+infixr 2 @:@
+(@:@) :: Monad m => Object (Singleton x) m -> Object (Union ys) m -> Object (Union (x : ys)) m
+objL @:@ objR = Object $
+  (\xv -> second (@:@ objR) <$> runObject objL (UNow xv))
+  @> (\ys -> second (objL @:@) <$> runObject objR ys)
+
+wfLayered :: FilePath -> V2 Int -> Object (Union xs) GameM -> Object (Union (Op'Render : xs)) GameM
+wfLayered path v obj = wLayer path v @:@ obj
 
 -- Delayed
 
-data Delayed a
-  = Delayed
-  { _delayed :: a
-  , _counter :: Int
+data Delay
+  = Delay
+  { _counter :: Int
   , _delayCount :: Int
   }
   deriving (Eq, Show)
 
-makeLenses ''Delayed
+makeLenses ''Delay
 
+{-
 newDelayed :: Int -> a -> Delayed a
 newDelayed n ma = Delayed ma 0 n
 
@@ -143,12 +150,23 @@ runDelayed ma delay = do
   ma' <- if delay^.counter == 0 then ma (delay^.delayed) else return (delay^.delayed)
   return $ delay & delayed .~ ma'
                  & counter %~ (`mod` (delay^.delayCount)) . (+1)
+-}
 
-data Eff'Delayed eff this (m :: * -> *) r where
-  New'Delayed :: Int -> eff that GameM this -> Eff'Delayed eff that GameM (Delayed this)
-  Step'Delayed :: (this -> eff that GameM this) -> Delayed this -> Eff'Delayed eff that GameM (Delayed this)
-  Map'Delayed :: (this -> eff that m this) -> Delayed this -> Eff'Delayed eff that m (Delayed this)
+data Op'Run r where
+  Op'Run :: Op'Run ()
 
+wDelay :: Int -> Object (Singleton Op'Run) GameM
+wDelay n = Delay 0 n @~ singleton (\Op'Run -> go) where
+  go :: Monad m => StateT Delay m ()
+  go = do
+    c <- use counter
+    d <- use delayCount
+    counter %= (`mod` d) . (+1)
+
+wfDelayed' :: Member xs Op'Run => Int -> Object (Union xs) (GameM `Sum` Union xs)
+wfDelayed' n = _
+
+{-
 wfDelayed :: Widget (eff that) m r -> Widget (Eff'Delayed eff that) m r
 wfDelayed widget = await >>= \case
   New'Delayed n new ->
@@ -157,4 +175,4 @@ wfDelayed widget = await >>= \case
     for (yield (run (this^.delayed)) >-> widget) $ \r -> lift (runDelayed (\t -> return r) this) >>= yield
   Map'Delayed op this ->
     for (yield (op (this^.delayed)) >-> widget) $ \w -> yield $ this & delayed .~ w
-
+-}
