6828d858319be4bbee76b3ea4460c4bd81895ef5
myuon <ioi.joi.koi.loi@gmail.com>
2017-07-27 00:41:45 +0900

    declare {value/self} op getter

--- SDLight/Widgets/TH.hs
+++ SDLight/Widgets/TH.hs
@@ -3,8 +3,10 @@ module SDLight.Widgets.TH
   ) where
 
 import Control.Lens
+import Control.Monad
 import Data.Char
 import Language.Haskell.TH
+import SDLight.Widgets.Core
 
 data Operator
   = Operator
@@ -47,26 +49,88 @@ declareOpDatatype opr = do
     ]
 
 declareOpGetter :: Operator -> DecsQ
-declareOpGetter opr = do
-  xsName <- newName "xs"
-  let opName = mkName $ (\s -> let (a,b) = splitAt 4 s in fmap toLower a ++ b) $ show $ opr^.name
+declareOpGetter opr = case opr^.branch of
+  ConT c | c == ''Self -> declareOpSelfGetter opr
+  ConT c | c == ''Value -> declareOpValueGetter opr
+  _ -> declareOpOtherGetter opr
 
-  {- op'piyo :: Op'Piyo ∈ xs => A -> B -> Getter (Widget xs) (FreezeT (Widget xs) Identity ())
-     op'piyo = _Op Op'Piyo -}
-  let wxs = ConT (mkName "Widget") `AppT` VarT xsName
-  return $
-    [ SigD opName
-      $ ForallT [] [(ConT (mkName "∈")) `AppT` ConT (opr^.name) `AppT` VarT xsName]
-      $ buildArr (opr^.args)
-      $ ConT (mkName "Getter")
-      `AppT` wxs
-      `AppT` ((opr^.branch) `AppT` wxs `AppT` (opr^.monad) `AppT` (opr^.rettype))
-    , FunD opName
-      [ Clause []
-        (NormalB $ VarE (mkName "_Op") `AppE` ConE (opr^.name))
-        []
-      ]
-    ]
+  where
+    declareOpOtherGetter :: Operator -> DecsQ
+    declareOpOtherGetter opr = do
+      xsName <- newName "xs"
+      let opName = mkName $ (\s -> let (a,b) = splitAt 4 s in fmap toLower a ++ b) $ show $ opr^.name
+
+      {- op'piyo :: Op'Piyo ∈ xs => A -> B -> Getter (Widget xs) (FreezeT (Widget xs) Identity ())
+         op'piyo a b = _Op (Op'Piyo a b) -}
+      let wxs = ConT (mkName "Widget") `AppT` VarT xsName
+      vars <- replicateM (length $ opr^.args) $ newName "x"
+
+      return $
+        [ SigD opName
+          $ ForallT [] [(ConT (mkName "∈")) `AppT` ConT (opr^.name) `AppT` VarT xsName]
+          $ buildArr (opr^.args)
+          $ ConT (mkName "Getter")
+          `AppT` wxs
+          `AppT` ((opr^.branch) `AppT` wxs `AppT` (opr^.monad) `AppT` (opr^.rettype))
+        , FunD opName
+          [ Clause
+            (fmap VarP vars)
+            (NormalB $ VarE (mkName "_Op") `AppE` foldl (\e n -> e `AppE` VarE n) (ConE (opr^.name)) vars)
+            []
+          ]
+        ]
+
+    declareOpSelfGetter :: Operator -> DecsQ
+    declareOpSelfGetter opr = do
+      let isIdentity = opr^.monad == ConT ''Identity
+      xsName <- newName "xs"
+      let opName = mkName $ (\s -> let (a,b) = splitAt 4 s in fmap toLower a ++ b) $ show $ opr^.name
+
+      {- op'piyo :: Op'Piyo ∈ xs => A -> B -> Getter (Widget xs) (m (Widget xs))
+         op'piyo a b = _self (Op'Piyo a b) -}
+      let wxs = ConT (mkName "Widget") `AppT` VarT xsName
+      vars <- replicateM (length $ opr^.args) $ newName "x"
+
+      return $
+        [ SigD opName
+          $ ForallT [] [(ConT (mkName "∈")) `AppT` ConT (opr^.name) `AppT` VarT xsName]
+          $ buildArr (opr^.args)
+          $ ConT (mkName "Getter")
+          `AppT` wxs
+          `AppT` (if isIdentity then id else AppT (opr^.monad)) wxs
+        , FunD opName
+          [ Clause
+            (fmap VarP vars)
+            (NormalB $ VarE (mkName $ if isIdentity then "_self'" else "_self") `AppE` foldl (\e n -> e `AppE` VarE n) (ConE (opr^.name)) vars)
+            []
+          ]
+        ]
+
+    declareOpValueGetter :: Operator -> DecsQ
+    declareOpValueGetter opr = do
+      let isIdentity = opr^.monad == ConT ''Identity
+      xsName <- newName "xs"
+      let opName = mkName $ (\s -> let (a,b) = splitAt 4 s in fmap toLower a ++ b) $ show $ opr^.name
+
+      {- op'piyo :: Op'Piyo ∈ xs => A -> B -> Getter (Widget xs) (m r)
+         op'piyo a b = _value (Op'Piyo a b) -}
+      let wxs = ConT (mkName "Widget") `AppT` VarT xsName
+      vars <- replicateM (length $ opr^.args) $ newName "x"
+
+      return $
+        [ SigD opName
+          $ ForallT [] [(ConT (mkName "∈")) `AppT` ConT (opr^.name) `AppT` VarT xsName]
+          $ buildArr (opr^.args)
+          $ ConT (mkName "Getter")
+          `AppT` wxs
+          `AppT` (if isIdentity then id else AppT (opr^.monad)) (opr^.rettype)
+        , FunD opName
+          [ Clause
+            (fmap VarP vars)
+            (NormalB $ VarE (mkName $ if isIdentity then "_value'" else "_value") `AppE` foldl (\e n -> e `AppE` VarE n) (ConE (opr^.name)) vars)
+            []
+          ]
+        ]
 
 parseWithWild :: Name -> Type -> Operator
 parseWithWild n = getArgs getOpTypes
@@ -83,4 +147,4 @@ makeOp name typQ = typQ >>= \typ -> do
   let n = mkName $ "Op'" ++ name
   let opr = parseWithWild n typ
   fmap concat $ sequence [declareOpDatatype opr, declareOpGetter opr]
- 
+
